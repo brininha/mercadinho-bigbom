2c9a636f3ce4e476335c7e1ccdd1a47fe44a85ad
Sabrina Cristan <sabrinacristan@gmail.com>
2023-04-30 12:22:15 -0300

    adicionando crud

--- crud/src/connection.ts
+++ crud/src/connection.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const URI = 'mongodb://localhost:27017';
+const URI = 'mongodb://localhost:27017/banquinho';
 
 async function connectDB() {
-    const DB = await mongoose.connect(URI, { dbName: 'banquinho' })
+    const DB = await mongoose.connect(URI)
     console.log(`${mongoose.connection.db.databaseName} connected!`)
 }
 
