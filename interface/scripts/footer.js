2ea7f6cea745229a3148cf7b3c220f75c86a5da9
Sabrina Cristan <sabrinacristan@gmail.com>
2023-05-01 12:58:27 -0300

    adicionando interface

--- interface/scripts/footer.js
+++ interface/scripts/footer.js
@@ -1,4 +1,4 @@
 // HTML do rodapé
-var footer = '<p class="text-center" style="color: #FFF;">&copy Todos os direitos reservados</p>';
+var footer = '<div class="footer-img"><img src="./images/iniciais.png" width="50px"></div><p class="text-center" style="color: #FFF;">&copy Todos os direitos reservados</p>';
 // Inserção do menu nas páginas
 document.getElementById("footer").innerHTML = footer;
